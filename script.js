33b279b2802bf9e127d4b0811484eb14eb5cb65b
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-24 21:35:40 -0300

    34234

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let val = document.getElementById('valor');
 let senha = document.getElementById('password');
 let resultado = document.getElementsByClassName('result');
 let apagao = document.getElementById('semtela')
+let sucesso = document.getElementById('copy')
 
 let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@';
 let novaSenha = '';
@@ -19,9 +20,14 @@ function gerar(){
         pass += charset.charAt(Math.floor(Math.random()* n));
     }
 
-    apagao.style.display = 'block'
-    senha.innerHTML = `${pass}`
-    console.log(pass)
-    
+    apagao.style.display = 'block';
+    senha.innerHTML = `${pass}`;
+    console.log(pass);
+    novaSenha = pass;
+    sucesso.style.display = 'none'
+}
 
+function copysenha(){
+    navigator.clipboard.writeText(novaSenha);
+    sucesso.style.display = 'block'
 }
