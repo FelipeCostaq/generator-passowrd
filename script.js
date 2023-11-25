b33c9377f32551cfe2abf4c5631086dfadbfdfd4
Felipe Costa <felipecostasiqu@gmail.com>
2023-11-24 21:29:03 -0300

    314

--- script.js
+++ script.js
@@ -3,16 +3,25 @@ let but = document.getElementById('gerador');
 let val = document.getElementById('valor');
 let senha = document.getElementById('password');
 let resultado = document.getElementsByClassName('result');
+let apagao = document.getElementById('semtela')
 
-let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@';
 let novaSenha = '';
 
 val.innerHTML = ranger.value
-ran.oninput = function(){
+ranger.oninput = function(){
     val.innerHTML = this.value
 }
 
 function gerar(){
+    let pass = ''
+    for(let i = 0, n = charset.length; i < ran.value; ++i){
+        pass += charset.charAt(Math.floor(Math.random()* n));
+    }
+
+    apagao.style.display = 'block'
+    senha.innerHTML = `${pass}`
+    console.log(pass)
     
 
 }
